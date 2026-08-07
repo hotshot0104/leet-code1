9e3ed487da17f3728d8df843edb16755f269e12e
Sameer Kumar Singh <sameersingh3740@gmail.com>
2026-08-07 15:48:18 +0530

    Time: 0 ms (100%), Space: 42 MB (71.03%) - LeetHub

---
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int fib(int n) {
+        int a =0;
+        int b=1;
+        int c=0;
+        if(n==1)
+        return 1;
+        for(int i=0;i<n-1;i++)
+        {
+            c=a+b;
+            a=b;b=c;
+        }
+        return c;
+
+        
+    }
+}
